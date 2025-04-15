8477c6b144b80ca284eeb1f774fa8f9e624a0add
91830 <91830@LAPTOP-JPHJ2F9N>
2025-04-15 19:56:37 +0530

    new changes

--- AdvanceSelenium_E18/src/main/java/GenericUtility/ExcelFileUtility.java
+++ AdvanceSelenium_E18/src/main/java/GenericUtility/ExcelFileUtility.java
@@ -10,7 +10,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 public class ExcelFileUtility {
 	
 	public String readingDataFromExcel(String sheet,int rowNum,int cellNum) throws EncryptedDocumentException, IOException {
-	FileInputStream fis=new FileInputStream("C:\\Users\\91830\\eclipse-workspace\\AdvanceSelenium_E18\\src\\test\\resources\\TestScriptData_E18.xlsx");
+	FileInputStream fis=new FileInputStream("C:\\Users\\91830\\GIT\\E18_Batch\\AdvanceSelenium_E18\\src\\test\\resources\\TestScriptData_E18.xlsx");
 	Workbook wb = WorkbookFactory.create(fis);
 	String data = wb.getSheet(sheet).getRow(rowNum).getCell(cellNum).getStringCellValue();
 	return data;
--- AdvanceSelenium_E18/src/main/java/GenericUtility/PropertiesFileUtility.java
+++ AdvanceSelenium_E18/src/main/java/GenericUtility/PropertiesFileUtility.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class PropertiesFileUtility {
 	public String readDataFromPropFile(String Key) throws IOException
 	{
-		FileInputStream fis=new FileInputStream("C:\\Users\\91830\\eclipse-workspace\\AdvanceSelenium_E18\\src\\test\\resources\\CommonData.properties");
+		FileInputStream fis=new FileInputStream("C:\\Users\\91830\\GIT\\E18_Batch\\AdvanceSelenium_E18\\src\\test\\resources\\CommonData.properties");
 		Properties prop=new Properties();
 		prop.load(fis);
 		String data = prop.getProperty(Key);
